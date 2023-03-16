81074ea151d9c5a8b543eb1405158dc02307a5f0
Gabrijel <g.gordic@outlook.com>
2023-03-16 20:19:12 +0100

    svasta

---
+++ py_func_03.py
@@ -0,0 +1,29 @@
+import os 
+
+
+def get_full_name(first_name: str, 
+                  last_name: str, 
+                  nick_name:str) -> str:
+    first_name = first_name.capitalize()
+    last_name = last_name.capitalize()
+    nick_name = nick_name.capitalize()
+    
+    return f'{first_name} {last_name} {nick_name}'
+
+#full_name = get_full_name('Pero', 'Peric', 'Pere')
+first_name = input('Upisite ime: ')
+last_name = input('Upisite prezime: ')
+nick_name = input('Upisite nadimak: ')
+
+full_name = get_full_name(last_name=last_name,
+                          nick_name=nick_name, 
+                          first_name=first_name)
+
+#if os.name == 'ne':
+    #os.system('cls')
+#else:
+    #os.system('clear')
+
+os.system('cls' if os.name =='nt' else 'clear')
+print(full_name)
+print()
--- py_rock_paper_scissors.py
+++ py_rock_paper_scissors.py
@@ -12,7 +12,7 @@ pieces = {1: 'kamen',
           }
 
 
-def menu():
+def menu() -> None:
     os.system('cls')
     print('*' * 50)
     print()
@@ -20,6 +20,24 @@ def menu():
         print(f'{key}. {value}')
     print()
     print('*' * 50)
+
+def message(message: str, destination: str):
+    match destination:
+        case 'console':
+            print(message)
+        case 'db_':
+            pass
+        case 'txt_file':
+            pass
+        case _:
+            pass
+    
+    pass
+
+
+# Funkcija za unos
+# Funkcija za provjeru str
+# Funkcija za provjeru int
 
 
 def user_input_validation(user_input: str) -> int:
@@ -36,37 +54,39 @@ def user_input_validation(user_input: str) -> int:
 
     return int(user_input)
 
-def check_game_status():
-    return True
+def check_game_status(computer_choice: int, user_choice: int) -> None:
+    destination = 'console'
+    
+    if computer_choice == user_choice:
+        message(
+        print(f'Nerijeseno  - Vas izbor {pieces[user_choice]} je isti kao {pieces[computer_choice]}', destination))
+    elif computer_choice == 1 and user_choice == 2:
+        message(
+        print(f'Izgubili ste  - Vas izbor {pieces[user_choice]} gubi od {pieces[computer_choice]}', destination))
+    elif computer_choice == 1 and user_choice == 3:
+        message(
+        print(f'Pobijedili ste  - Vas izbor {pieces[user_choice]} pobjeduje {pieces[computer_choice]}', destination))
+    elif computer_choice == 2 and user_choice == 1:
+        message(
+        print(f'Pobijedili ste  - Vas izbor {pieces[user_choice]} pobjeduje {pieces[computer_choice]}', destination))
+    elif computer_choice == 2 and user_choice == 3:
+        message(
+        print(f'Izgubili ste  - Vas izbor {pieces[user_choice]} gubi od {pieces[computer_choice]}', destination))
+    elif computer_choice == 3 and user_choice == 1:
+        message(
+        print(f'Izgubili ste  - Vas izbor {pieces[user_choice]} gubi od {pieces[computer_choice]}', destination))
+    elif computer_choice == 3 and user_choice == 2:
+        message(
+        print(f'Pobijedili ste  - Vas izbor {pieces[user_choice]} pobjeduje {pieces[computer_choice]}', destination))
+    
 
 while True:
     menu()
 
-    # region Players input -> computer and user
-
     user_choice = user_input_validation(input('Izaberite jedan broj iz izbornika : '))
     computer_choice = random.randint(1, 3)
-    #user_input_validation(user_choice)
-# endregion
 
-    # region Check game status
-
-    if computer_choice == user_choice:
-        print(f'Nerijeseno  - Vas izbor {pieces[user_choice]} je isti kao {pieces[computer_choice]}')
-    elif computer_choice == 1 and user_choice == 2:
-        print(f'Izgubili ste  - Vas izbor {pieces[user_choice]} gubi od {pieces[computer_choice]}')
-    elif computer_choice == 1 and user_choice == 3:
-        print(f'Pobijedili ste  - Vas izbor {pieces[user_choice]} pobjeduje {pieces[computer_choice]}')
-    elif computer_choice == 2 and user_choice == 1:
-        print(f'Pobijedili ste  - Vas izbor {pieces[user_choice]} pobjeduje {pieces[computer_choice]}')
-    elif computer_choice == 2 and user_choice == 3:
-        print(f'Izgubili ste  - Vas izbor {pieces[user_choice]} gubi od {pieces[computer_choice]}')
-    elif computer_choice == 3 and user_choice == 1:
-        print(f'Izgubili ste  - Vas izbor {pieces[user_choice]} gubi od {pieces[computer_choice]}')
-    elif computer_choice == 3 and user_choice == 2:
-        print(f'Pobijedili ste  - Vas izbor {pieces[user_choice]} pobjeduje {pieces[computer_choice]}')
-
-    # endregion
+    check_game_status(computer_choice, user_choice)
 
     next_round = input('Zelite li ponoviti igru? (Da/Ne)')
     if next_round[: 2].lower() != 'da':
