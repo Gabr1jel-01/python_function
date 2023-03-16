32718fb20b464b906cce00df18431b1fc7d2d599
Gabrijel <g.gordic@outlook.com>
2023-03-16 19:09:50 +0100

    Update py_rock_paper_scissors.py

--- py_rock_paper_scissors.py
+++ py_rock_paper_scissors.py
@@ -6,41 +6,51 @@ Usporedimo izbore i ovisno o izboru prikazemo rezultat
 import random
 import os
 
+pieces = {1: 'kamen',
+          2: 'skare',
+          3: 'papir'
+          }
 
-pieces = { 1: 'kamen',
-           2: 'skare', 
-           3: 'papir'
-}
 
 def menu():
     os.system('cls')
+    print('*' * 50)
     print()
     for key, value in pieces.items():
         print(f'{key}. {value}')
     print()
+    print('*' * 50)
 
 
+def user_input_validation(user_input: str) -> int:
 
+    while not user_input.isnumeric():
+        print('Morate unijeti brojke')
+        user_input = input('Pokusajte unijeti ispravan broj iz izbornika ')
 
+    user_input = int(user_input)
+    while user_input < 1 or user_input > 3:
+        print('Krivi unos')
+        print('Unesite jedan od brojeva iz izbornika!')
+        user_input = int(input('Pokusajte unijeti ispravan broj iz izbornika '))
+
+    return int(user_input)
+
+def check_game_status():
+    return True
 
 while True:
     menu()
-    
-    #region Players input -> computer and user
-    user_choice = input('Izaberite jedan broj iz izbornika : ')
-    while not user_choice.isnumeric():
-        print('Morate unijeti brojke')
-        user_choice = input('Pokusajte unijeti ispravan broj iz izbornika ')
-    
-    user_choice = int(user_choice)
-    while int(user_choice) < 1 or int(user_choice) > 3:
-        print('Krivi unos')
-        print('Unesite jedan od brojeva iz izbornika!')
-        user_choice = int(input('Pokusajte unijeti ispravan broj iz izbornika '))
-#endregion
 
-    #region Check game status
-    computer_choice = random.randint(1,3)
+    # region Players input -> computer and user
+
+    user_choice = user_input_validation(input('Izaberite jedan broj iz izbornika : '))
+    computer_choice = random.randint(1, 3)
+    #user_input_validation(user_choice)
+# endregion
+
+    # region Check game status
+
     if computer_choice == user_choice:
         print(f'Nerijeseno  - Vas izbor {pieces[user_choice]} je isti kao {pieces[computer_choice]}')
     elif computer_choice == 1 and user_choice == 2:
@@ -55,13 +65,12 @@ while True:
         print(f'Izgubili ste  - Vas izbor {pieces[user_choice]} gubi od {pieces[computer_choice]}')
     elif computer_choice == 3 and user_choice == 2:
         print(f'Pobijedili ste  - Vas izbor {pieces[user_choice]} pobjeduje {pieces[computer_choice]}')
-        
-    #endregion
-    
-    
-    next_round = input('Zelite li ponoviti igru? (Da/Ne)')
-    if next_round[ : 2].lower() != 'da':
-        break
-    
 
-# Pozdravna poruka 
+    # endregion
+
+    next_round = input('Zelite li ponoviti igru? (Da/Ne)')
+    if next_round[: 2].lower() != 'da':
+        break
+
+
+# Pozdravna poruka
